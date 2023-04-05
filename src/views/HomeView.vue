0e333d24e50a79063fdd888eb3dab8a807a1c24a
Felipe Souza <felipemarlon.san@gmail.com>
2023-04-05 20:18:33 -0300

    chore: alterado estado da store de produtos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,24 +8,24 @@ const errorImage =
 
 const productStore = useProductStore()
 
-const { products, loading, error } = storeToRefs(productStore)
+const { state } = storeToRefs(productStore)
 
 productStore.getProducts()
 </script>
 
 <template>
-  <div v-if="loading" class="d-flex" style="height: 35rem">
+  <div v-if="state.loading" class="d-flex" style="height: 35rem">
     <div class="spinner-border m-auto text-primary" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
   </div>
-  <div v-else-if="error" class="d-flex" style="height: 35rem">
+  <div v-else-if="state.error" class="d-flex" style="height: 35rem">
     <div class="card m-auto" style="width: 18rem">
       <img :src="errorImage" class="card-img-top" alt="..." />
       <div class="card-body">
         <h5 class="card-title">Ops</h5>
         <p class="card-text">
-          {{ error }}
+          {{ state.error }}
         </p>
         <button @click="($event) => productStore.getProducts()" class="btn btn-primary">
           Try Again
@@ -35,7 +35,11 @@ productStore.getProducts()
   </div>
 
   <div v-else class="row">
-    <div v-for="item in products" :key="item.id" class="col-sm-12 col-md-4 col-lg-3 col-xl-2 mb-4">
+    <div
+      v-for="item in state.products"
+      :key="item.id"
+      class="col-sm-12 col-md-4 col-lg-3 col-xl-2 mb-4"
+    >
       <ProductCard />
     </div>
   </div>
