e5e1bf5cedac879f4ae2117fa30beb1f492fa299
Felipe Souza <felipemarlon.san@gmail.com>
2023-04-07 12:31:14 -0300

    chore: adicionado banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import { storeToRefs } from 'pinia'
 import ProductCard from '../components/ProductCard.vue'
 import { useProductStore } from '../stores/products'
-
+import banner from '@/assets/banner.jpg'
 const errorImage =
   'https://uploads.sitepoint.com/wp-content/uploads/2015/12/1450973046wordpress-errors.png'
 
@@ -14,12 +14,16 @@ productStore.getProducts()
 </script>
 
 <template>
-  <div v-if="state.loading" class="d-flex" style="height: 35rem">
+  <div>
+    <img class="img-fluid rounded-bottom-2 mb-3" :src="banner" />
+  </div>
+
+  <div v-if="state.loading" class="d-flex" style="height: 10rem">
     <div class="spinner-border m-auto text-primary" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
   </div>
-  <div v-else-if="state.error" class="d-flex" style="height: 35rem">
+  <div v-else-if="state.error" class="d-flex" style="height: 10rem">
     <div class="card m-auto" style="width: 18rem">
       <img :src="errorImage" class="card-img-top" alt="..." />
       <div class="card-body">
@@ -34,13 +38,16 @@ productStore.getProducts()
     </div>
   </div>
 
-  <div v-else class="row">
-    <div
-      v-for="item in state.products"
-      :key="item.id"
-      class="col-sm-12 col-md-4 col-lg-3 col-xl-2 mb-4"
-    >
-      <ProductCard />
+  <div v-else class="">
+    <h3>New Items</h3>
+    <div class="row">
+      <div
+        v-for="item in state.products"
+        :key="item.id"
+        class="col-sm-12 col-md-4 col-lg-3 col-xl-2 mb-4"
+      >
+        <ProductCard :product="item" />
+      </div>
     </div>
   </div>
 </template>
